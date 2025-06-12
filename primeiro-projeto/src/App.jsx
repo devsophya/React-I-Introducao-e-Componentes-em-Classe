a2d9537c38c0bc40a25d4273bbada6f0bba6ae0d
Sophya <sophyadamiazodev@gmail.com>
2025-06-12 17:29:58 -0300

    feat: add Navbar component with styles and global app styles.

---
+++ primeiro-projeto/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles/App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+class App extends React.Component {
+  render() {
+    return <Navbar />;
+  }
+}
+
+export default App;
